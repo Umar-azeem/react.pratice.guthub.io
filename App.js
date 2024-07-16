d79ad82f94a01d5c1b3d7ba16b898c65e2dcf290
umar azeem <125313441+Umar-azeem@users.noreply.github.com>
2024-07-16 09:23:55 -0700

    Update App.js

--- App.js
+++ App.js
@@ -6,18 +6,19 @@ import './App.css';
 function App() {
   const [age, setAge] = useState();
   const [print, setPrint] = useState();
-  const [value,setValue] = useState('')
-  const option = useMemo (()=>countryList(),[])
+  const [value,setValue] = useState('');
+  const option = useMemo ( ()=> countryList().getData(),[])
   const changeHandler = value =>{
     setValue(value)
   }
-
+ 
 
   
    function showOutputFunctionForVote() {
     const showValue = 
-       age >= 18 ? "You are eligible to vote." : "you are note eligible for vote." 
+       age >= 18 && value === "pakistan" ? "You are eligible to vote." : "you are note eligible for vote." 
        setPrint(showValue)
+       
     
    }
     
@@ -28,20 +29,13 @@ function App() {
      <section className='w-screen h-screen bg-black'>
      <div className='w-full h-auto flex  justify-center'>
       
-
-
-
-      <input
-        value={age} 
-        onChange={(e) => setAge(e.target.value)}
-        className="w-2/4 h-14 text-2xl m-10  font-bold rounded-full"
-        placeholder="Enter your age" 
-        type="number"
-      />
-      <Select options={option} value={value} onChange={changeHandler}/>
+     <div class=" m-10 space-y-3">
+  <input value={age} onChange={(e) => setAge(e.target.value)} placeholder="Enter your age" type="number" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"/>
+    </div>
+      <Select className="w-96 m-10" options={option} value={value} onChange={changeHandler}/>
        <button onClick={showOutputFunctionForVote} className="color w-28 text-black hover:text-white hover:bg-blue-900 hover:translate-y-2 m-10 h-12 rounded-sm bg-blue-400" >show</button>
-      <p className="w-2/4 h-10 text-2xl text-white m-10 text-start bg-gray-400 font-bold rounded-sm">{print}</p>
       </div>
+      <p className="w-2/4 h-10 text-2xl text-white m-10 text-start bg-gray-400 font-bold rounded-sm">{print}</p>
 
 
      </section>
